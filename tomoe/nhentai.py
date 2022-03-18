03f6e682a3d166b4238b3dc97ebe5b0058d12bd4
d0g <bibsindra@gmail.com>
2022-03-19 02:05:18 +0700

    feat: add convert to pdf feature

--- tomoe/nhentai.py
+++ tomoe/nhentai.py
@@ -4,12 +4,14 @@ import requests
 import os
 import re
 import time
-from .utils.misc import choose, split_name
+from .utils.misc import choose, split_name, get_size, convert_html_to_pdf, project
+from inputimeout import inputimeout, TimeoutOccurred
 
 nh = janda.Nhentai()
 
 
 async def get_nh(id: int = choose().nhentai):
+    initial = time.time()
     data = await nh.get(id)
     parser = json.loads(data)
 
@@ -25,7 +27,7 @@ async def get_nh(id: int = choose().nhentai):
     if not os.path.exists(neat_dir):
         os.makedirs(neat_dir)
 
-    if len(os.listdir(neat_dir)) - 1 == len(img):
+    if len(os.listdir(neat_dir)) >= len(img):
         print("All images already downloaded! If you're doubt kindly remove this folder and re-download")
         print(f'Directory: {os.path.abspath(neat_dir)}')
         return
@@ -41,15 +43,49 @@ async def get_nh(id: int = choose().nhentai):
 
             if os.path.exists(neat_dir + '/' + img_name):
                 print(
-                    f'Successfully downloaded {img_name} | in {time.time() - start:.2f} seconds')
+                    f'Successfully downloaded {img_name} | {get_size(neat_dir + "/" + img_name)} MB | in {time.time() - start:.2f} seconds')
 
             if len(img) == len(os.listdir(neat_dir)):
-                print("All images downloaded!")
+                print(
+                    f'Successfully downloaded all images in {(time.time() - initial) / 60:.2f} minutes')
                 with open(neat_dir + '/gallery.html', 'w') as f:
-                    f.write('<html><body>')
-                    for i in os.listdir(neat_dir):
-                        f.write(f'<img src="{i}">')
-                    f.write('</body></html>')
+                    f.write('<html><center><body>')
+                    f.write(f'<h1>{title}</h1>')
+                    ## print(os.listdir(neat_dir)[:-1])
+                    for i in os.listdir(neat_dir)[:-1]:
+                        ## write all images with valid name and path
+                        f.write(f'<img src="{neat_dir}/{i}"><p></p>')
+                    f.write(f'{project()}')
+                    f.write('</body></center></html>')
 
                     print(f'Static gallery saved to {neat_dir}/gallery.html')
                     print(f'Directory: {os.path.abspath(neat_dir)}')
+
+                ##to_pdf = input("Do you want to convert to pdf aswell? (y/n) ")
+                try:
+                    desired = inputimeout(prompt='Do you want to convert to pdf aswell? (y/n) ', timeout=10)
+                    
+                except TimeoutOccurred:
+                    print("Timeout occurred")
+                    exit()
+
+                to_pdf = desired
+                print(to_pdf)
+
+                if to_pdf == 'y':
+                    try:
+                        source = open(f"{neat_dir}/gallery.html")
+                        output = f"{neat_dir}/gallery.pdf"
+
+                        convert_html_to_pdf(source, output)
+                        print(f'Successfully converted to pdf')       
+
+                    except Exception as e:
+                        print(
+                            f"Something went wrong while converting to pdf: {e}")
+                                
+                elif to_pdf == 'n':
+                    print("Okay")
+                    os.remove(neat_dir + '/gallery.html')
+                else:
+                    print("Invalid input")
