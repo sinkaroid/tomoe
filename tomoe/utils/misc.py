5f20aac14b11426884f1636f7dea9f462e1d51db
d0g <bibsindra@gmail.com>
2022-03-19 02:04:18 +0700

    feat: uses xhtml2pdf for it's pdf converter

--- tomoe/utils/misc.py
+++ tomoe/utils/misc.py
@@ -1,8 +1,10 @@
 import argparse
 import os
+from xhtml2pdf import pisa
 
 def choose():
     my_parser = argparse.ArgumentParser()
+
     my_parser.add_argument('--nhentai', action='store', type=int)
     my_parser.add_argument('--pururin', action='store', type=int)
     my_parser.add_argument('--simply', action='store', type=str)
@@ -15,3 +17,29 @@ def choose():
 def split_name(string):
     get_name = os.path.basename(string).split('.')[0]
     return get_name
+
+def get_size(string):
+    file = round(os.path.getsize(string) / 1024 / 1024, 2)
+    return file
+
+def project():
+    return '<p><h2><a href="https://pypi.org/project/tomoe/">Generated from tomoe: (https://pypi.org/project/tomoe)</a><h2>'
+           
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+            source_html,                # the HTML to convert
+            dest=result_file)           # file handle to recieve result
+
+    # make the result is scrollable
+    pisa_status.dest.seek(0)
+    
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
