345c5b0fa6260b74649bb7c3fd484f0c4f8f38b6
sinkaroid <bibsindra@gmail.com>
2022-04-09 12:07:54 +0700

    chore: remove unused comments on `utils/misc`

--- tomoe/utils/misc.py
+++ tomoe/utils/misc.py
@@ -2,24 +2,44 @@ import argparse
 import os
 from xhtml2pdf import pisa
 
+
 def choose():
     my_parser = argparse.ArgumentParser()
 
-    my_parser.add_argument('--nhentai', '-nhentai', action='store', metavar=('ID'), type=int)
-    my_parser.add_argument('--pururin', '-pururin', action='store', metavar=('ID'), type=int)
-    my_parser.add_argument('--simply', '-simply', action='store', metavar=('PATH_AFTER_WWW'), type=str)
-    my_parser.add_argument('--hentaifox', '-hentaifox', action='store', metavar=('ID'), type=int)
-    my_parser.add_argument('--hentai2read', '-hentai2read', nargs=2, action='store', metavar=('PATH', 'CHAPTER'), type=str)
-    my_parser.add_argument('--qhentai', '-qhentai', action='store', metavar=('PATH_AFTER_WWW'), type=str)
-    my_parser.add_argument('--asmhentai', '-asmhentai', action='store', metavar=('ID'), type=int)
+    my_parser.add_argument(
+        "--nhentai", "-nhentai", action="store", metavar=("ID"), type=int
+    )
+    my_parser.add_argument(
+        "--pururin", "-pururin", action="store", metavar=("ID"), type=int
+    )
+    my_parser.add_argument(
+        "--simply", "-simply", action="store", metavar=("PATH_AFTER_WWW"), type=str
+    )
+    my_parser.add_argument(
+        "--hentaifox", "-hentaifox", action="store", metavar=("ID"), type=int
+    )
+    my_parser.add_argument(
+        "--hentai2read",
+        "-hentai2read",
+        nargs=2,
+        action="store",
+        metavar=("PATH", "CHAPTER"),
+        type=str,
+    )
+    my_parser.add_argument(
+        "--qhentai", "-qhentai", action="store", metavar=("PATH_AFTER_WWW"), type=str
+    )
+    my_parser.add_argument(
+        "--asmhentai", "-asmhentai", action="store", metavar=("ID"), type=int
+    )
 
-    
     args = my_parser.parse_args()
     return args
 
+
 def split_name(string):
-    """ Split string into name and extension 
-    
+    """Split string into name and extension
+
     Parameters
     ----------
     string : str
@@ -30,12 +50,13 @@ def split_name(string):
     name : str
         Name of the file
     """
-    get_name = os.path.basename(string).split('.')[0]
+    get_name = os.path.basename(string).split(".")[0]
     return get_name
 
+
 def get_size(string):
-    """ Get size of a file
-    
+    """Get size of a file
+
     Parameters
     ----------
     string : str
@@ -49,9 +70,10 @@ def get_size(string):
     file = round(os.path.getsize(string) / 1024 / 1024, 2)
     return file
 
+
 def get_size_folder(folder):
-    """ Get size of a folder
-    
+    """Get size of a folder
+
     Parameters
     ----------
     folder : str
@@ -69,13 +91,15 @@ def get_size_folder(folder):
             total_size += os.path.getsize(fp)
     return round(total_size / 1024 / 1024, 2)
 
+
 def project():
-    """ watermark hehe """
+    """watermark hehe"""
     return '<p><b><h1><a href="https://pypi.org/project/tomoe/">Generated from tomoe: https://pypi.org/project/tomoe</a></b><h1>'
-           
+
+
 def convert_html_to_pdf(source_html, output_filename):
-    """ Convert html to pdf
-    
+    """Convert html to pdf
+
     Parameters
     ----------
     source_html : str
@@ -85,17 +109,15 @@ def convert_html_to_pdf(source_html, output_filename):
     """
 
     result_file = open(output_filename, "w+b")
-    pisa_status = pisa.CreatePDF(
-            source_html,
-            dest=result_file)   
+    pisa_status = pisa.CreatePDF(source_html, dest=result_file)
 
-    ## pisa_status.dest.seek(0)
-    result_file.close()              
+    result_file.close()
     return pisa_status.err
 
+
 def nums(first_number, last_number, step=1):
-    """ Generate access index of a list
-    
+    """Generate access index of a list
+
     Parameters
     ----------
     first_number : int
@@ -104,17 +126,18 @@ def nums(first_number, last_number, step=1):
         Last number
     step : int
         Step
-    
+
     Returns
     -------
     list
         List of numbers
     """
-    return range(first_number, last_number+1, step)
+    return range(first_number, last_number + 1, step)
+
 
 def need_args():
-    """ Check if arguments are given 
-    
+    """Check if arguments are given
+
     Returns
     -------
     bool
