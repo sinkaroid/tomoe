a3a5c90907548dfdc29d327eea15187da496a290
sinkaroid <bibsindra@gmail.com>
2022-06-18 03:21:11 +0700

    chore(release): 3.0.10-alpha

--- tomoe/__init__.py
+++ tomoe/__init__.py
@@ -1,8 +1,9 @@
-__version__ = "3.0.7"
+__version__ = "3.0.10"
 from .pururin import get_pur
 from .nhentai import get_nh
 from .hentaifox import get_hfox
 from .hentai2read import get_h2r
 from .simplyh import get_sim
+from .asmhentai import get_asm
 from .cli import *
 from .utils.misc import choose
