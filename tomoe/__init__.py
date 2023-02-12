f009af1c7a7e2ad816b54207f5704327f64bb191
sinkaroid <bibsindra@gmail.com>
2023-02-12 07:42:15 +0700

    pre release

--- tomoe/__init__.py
+++ tomoe/__init__.py
@@ -1,10 +1,11 @@
-__version__ = "3.0.14"
+__version__ = "3.1.7"
 from .pururin import get_pur
 from .nhentai import get_nh
 from .hentaifox import get_hfox
 from .hentai2read import get_h2r
 from .simplyh import get_sim
 from .asmhentai import get_asm
+from .thentai import get_thentai
 from .bulk_download import get_bulk
 from .cli import *
 from .utils.misc import choose
