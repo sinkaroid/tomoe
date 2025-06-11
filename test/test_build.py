d9f6ffdcf1a9c96875b0890f1a376ef19c3a6cc6
Indrawan I. <bibsindra@gmail.com>
2025-06-12 05:04:13 +0700

    ci: sunset 3.8 & bump janda (#11)
    * ci: sunset 3.8, use 3.12
    
    * ci: unittest build
    
    * docs: update readme
    
    * deps: bump janda
    
    * chore: remake assets
    
    * chore(release): 3.7.1-alpha
    
    * ci: test workflows
    
    * cii
    
    * ci: proper cli setuptools

--- test/test_build.py
+++ test/test_build.py
@@ -1,2 +1,12 @@
+import unittest
 import tomoe
-print(tomoe.__version__)
+
+class TestVersion(unittest.TestCase):
+
+    def test_version(self):
+        print(tomoe.__version__)
+        self.assertIsInstance(tomoe.__version__, str)
+        self.assertRegex(tomoe.__version__, r"^\d+\.\d+\.\d+$")  # contoh: 1.0.0
+
+if __name__ == "__main__":
+    unittest.main()
